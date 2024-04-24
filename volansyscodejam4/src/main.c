30d1b75e51b734d3f7bea4dd511d642042254505
sneh.shah <sneh.shah@volansys.com>
2024-04-24 17:18:43 +0530

    added volansys code jam4

--- volansyscodejam4/src/main.c
+++ volansyscodejam4/src/main.c
@@ -2,48 +2,45 @@
 
 int main()
 {
-	int doll_count=0;
+	int doll_count = 0;
 	// printf("Hello World\n");
 
-	int arr[doll_count];
-	int test_case_count=0;
+	int test_case_count = 0;
 
 	printf("Enter test cases: ");
-	scanf("%d",&test_case_count);
+	scanf("%d", &test_case_count);
 
-	for (int k=0;k<test_case_count;k++)
+	for (int k = 0; k < test_case_count; k++)
 	{
 
-		printf("Enter no. of dolls: \n");
-		scanf(" %d",&doll_count);
+		printf("Enter no. of dolls: ");
+		scanf(" %d", &doll_count);
+		int arr[doll_count];
 
 		/* int visited_node[100]; */
 		/* int visited_count=0; */
-		for(int i=0;i<doll_count;i++)
+		for (int i = 0; i < doll_count; i++)
 		{
-			scanf("%d",&arr[i]);
+			scanf(" %d", &arr[i]);
 		}
 
-
-
-		int temp_count=0;
-		for(int i=0;i<doll_count;i++)
+		for (int i = 0; i < doll_count; i++)
 		{
+			int temp_count = 0;
 			// int temp=arr[i];
-			for(int j=0;i<doll_count;j++)
+			for (int j = 0; j < doll_count; j++)
 			{
-				if(arr[i]==arr[j])
+				if (arr[i] == arr[j])
 				{
 					temp_count++;
 				}
 			}
-			if(temp_count%2!=0)
+			if (temp_count % 2 != 0)
 			{
-				printf("%d is not paired",arr[i]);
+				printf("%d is not paired", arr[i]);
 				// return 0;
 			}
 		}
-
 	}
 	return 0;
 }
