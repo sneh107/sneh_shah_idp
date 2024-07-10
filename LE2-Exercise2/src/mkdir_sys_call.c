2b2f7a2b60574b32f3c3b7cf08320a5334deffe3
sneh.shah <sneh.shah@volansys.com>
2024-07-10 15:45:44 +0530

    update

--- LE2-Exercise2/src/mkdir_sys_call.c
+++ LE2-Exercise2/src/mkdir_sys_call.c
@@ -1,8 +1,34 @@
+/*******************************************************************************
+ * Copyright(c) 2024, Volansys Technologies
+ *
+ * Description:
+ * @file mkdir_sys_call.c
+ *
+ * Author       - Sneh Shah
+ *
+ *******************************************************************************
+ *
+ * History
+ *
+ * May/31/2024, Sneh Shah, Created
+ *
+ ******************************************************************************/
+
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
 const char *directories[] = {"a", "b", "c", "d", "e"};
+
+/**
+ * @brief Main function to create directories.
+ *
+ * The function iterates over the array of directory names and attempts to create each directory
+ * with read, write, and execute permissions for the owner, group, and others. It prints a success
+ * message for each directory created and an error message if the creation fails.
+ *
+ * @return int Returns 0 on successful completion.
+ */
 
 int main()
 {
